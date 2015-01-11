eeec976abeb4830e6de40c0f5f5c959fe9c354f8
Sergey Yuzepovich <yuzepovich@gmail.com>
2015-01-11 16:37:06 +0300

    added container view to entityView
    enhancing animation is now done on container

--- DynamicsOrientation/EntityView.m
+++ DynamicsOrientation/EntityView.m
@@ -11,11 +11,12 @@
 
 @implementation EntityView
 {
+    UIView *container;
     UIImageView *imageView;
     CGFloat initialSize;
     BOOL isExpanded;
-    NSLayoutConstraint *selfWidth;
-    NSLayoutConstraint *selfHeight;
+    NSLayoutConstraint *containerWidth;
+    NSLayoutConstraint *containerHeight;
     NSArray *behaviours;
 }
 
@@ -37,16 +38,16 @@
     imageView = [[UIImageView alloc]initWithImage:image];
     [imageView setFrame:self.frame];
     
-    [self addSubview: imageView];
+    [container addSubview: imageView];
 
     imageView.translatesAutoresizingMaskIntoConstraints = NO;
 
-    NSLayoutConstraint *centeredX = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0];
+    NSLayoutConstraint *centeredX = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:container attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0];
     NSLayoutConstraint *imageWidth = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetWidth(imageView.frame)];
     NSLayoutConstraint *imageHeight = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetHeight(imageView.frame)];
-    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:container attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0];
 
-    [self addConstraints:@[centeredX,top]];
+    [container addConstraints:@[centeredX,top]];
     [imageView addConstraints:@[imageWidth,imageHeight]];
     imageView.layer.masksToBounds = YES;
     imageView.layer.cornerRadius = CGRectGetWidth(self.bounds)/5;
@@ -56,42 +57,52 @@
 {
     self.layer.masksToBounds = YES;
     self.layer.cornerRadius = CGRectGetWidth(self.bounds)/5;
-    self.backgroundColor = [UIColor grayColor];
-
+    self.clipsToBounds = NO;
+    
     self.translatesAutoresizingMaskIntoConstraints = NO;
-    selfWidth = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetWidth(self.frame)];
-    selfHeight = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetHeight(self.frame)];
+
+    container = [[UIView alloc]init];
+    container.layer.masksToBounds = YES;
+    container.layer.cornerRadius = CGRectGetWidth(self.bounds)/5;
+    [self addSubview:container];
+    container.translatesAutoresizingMaskIntoConstraints = NO;
+    container.backgroundColor = [UIColor lightGrayColor];
+
+    containerWidth = [NSLayoutConstraint constraintWithItem:container attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetWidth(self.frame)];
+    containerHeight = [NSLayoutConstraint constraintWithItem:container attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetHeight(self.frame)];
+    [container addConstraints:@[containerWidth,containerHeight]];
+    
+    NSLayoutConstraint *selfWidth = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetWidth(self.frame)];
+    NSLayoutConstraint *selfHeight = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:0 multiplier:1.0 constant:CGRectGetHeight(self.frame)];
     [self addConstraints:@[selfWidth,selfHeight]];
+
     initialSize = CGRectGetWidth(self.frame);
     isExpanded = false;
+    
 }
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-//    POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewBounds];
-//    anim.toValue = [NSValue valueWithCGRect:isExpanded ? initialSize : CGRectMake(0, 0, 400, 400)];
-//    [self pop_addAnimation:anim forKey:@"size"];
-    
-    if(!isExpanded)
-    {
-        for(UIDynamicBehavior* beh in behaviours)
-        {
-            [beh performSelector:@selector(removeItem:) withObject:self];
-        }
-    }
-    else
-    {
-        for(UIDynamicBehavior* beh in behaviours)
-        {
-            [beh performSelector:@selector(addItem:) withObject:self];
-        }
-    }
+//    if(!isExpanded)
+//    {
+//        for(UIDynamicBehavior* beh in behaviours)
+//        {
+//            [beh performSelector:@selector(removeItem:) withObject:self];
+//        }
+//    }
+//    else
+//    {
+//        for(UIDynamicBehavior* beh in behaviours)
+//        {
+//            [beh performSelector:@selector(addItem:) withObject:self];
+//        }
+//    }
     POPSpringAnimation *layoutAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayoutConstraintConstant];
 //    layoutAnimation.springSpeed = 20.0f;
 //    layoutAnimation.springBounciness = 15.0f;
     layoutAnimation.toValue = @( isExpanded ? initialSize : 400 );
-    [selfWidth pop_addAnimation:layoutAnimation forKey:@"selfWidth"];
-    [selfHeight pop_addAnimation:layoutAnimation forKey:@"selfHeight"];
+    [containerWidth pop_addAnimation:layoutAnimation forKey:@"selfWidth"];
+    [containerHeight pop_addAnimation:layoutAnimation forKey:@"selfHeight"];
     isExpanded = !isExpanded;
 }
 
